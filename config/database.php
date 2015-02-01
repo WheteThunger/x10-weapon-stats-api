6a9232208e5e81758a776a1a232032c4872fb2c5
deviousfrog <thedeviousfrog@gmail.com>
2015-02-01 13:00:58 -0800

    Database config for pgsql will fall back to reading the env varible DATABASE_URL if the DB_* ones don't exist

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(env("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
 	/*
@@ -66,10 +73,10 @@ return [
 
 		'pgsql' => [
 			'driver'   => 'pgsql',
-			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'host'     => env('DB_HOST', $host),
+			'database' => env('DB_DATABASE', $database),
+			'username' => env('DB_USERNAME', $username),
+			'password' => env('DB_PASSWORD', $password),
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
